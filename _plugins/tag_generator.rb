40d1160043d81fc2479ecb4005246c1385e5e3c4
codebhav <vaibhava0801@gmail.com>
2025-05-16 16:57:08 -0400

    minor fix

--- _plugins/tag_generator.rb
+++ _plugins/tag_generator.rb
@@ -3,10 +3,25 @@ module Jekyll
     safe true
 
     def generate(site)
+      # Add debug output
+      puts "TagPageGenerator: Starting tag page generation"
+      
       if site.layouts.key? 'tag'
+        puts "TagPageGenerator: Found 'tag' layout"
         site.tags.each_key do |tag|
+          puts "TagPageGenerator: Generating page for tag '#{tag}'"
           site.pages << TagPage.new(site, site.source, File.join('writings', 'tags'), tag)
         end
+        
+        # Create a debug file that gets included in the build
+        debug_file = File.join(site.source, '_site', 'tag-debug.txt')
+        FileUtils.mkdir_p(File.dirname(debug_file))
+        File.open(debug_file, 'w') do |file|
+          file.puts "Tag generator ran at: #{Time.now}"
+          file.puts "Available tags: #{site.tags.keys.join(', ')}"
+        end
+      else
+        puts "TagPageGenerator: No 'tag' layout found!"
       end
     end
   end
@@ -22,6 +37,8 @@ module Jekyll
       self.read_yaml(File.join(base, '_layouts'), 'tag.html')
       self.data['tag'] = tag
       self.data['title'] = "Posts tagged with '#{tag}'"
+      
+      puts "TagPage: Created page for '#{tag}' at #{File.join(dir, @name)}"
     end
   end
 end
